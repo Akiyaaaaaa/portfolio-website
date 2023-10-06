8d279bf6b7e4a7b0b0cccb088c1ba4f678c34319
Zadid Taqwa <Zadidtaqwa123@gmail.com>
2023-10-07 01:34:32 +0700

    feat: add social media link

--- src/components/SocMed.jsx
+++ src/components/SocMed.jsx
@@ -1,17 +1,35 @@
 import { BsGithub, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 const SocMed = () => {
   return (
     <>
       <div className="app__social">
         <div>
-          <BsGithub />
+          <a
+            href="https://github.com/Akiyaaaaaa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsGithub />
+          </a>
         </div>
         <div>
-          <BsInstagram />
+          <a
+            href="https://instagram.com/kasuur.ruusak?igshid=YTQwZjQ0NmI0OA=="
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsInstagram />
+          </a>
         </div>
         <div>
-          <FaFacebookF />
+          <a
+            href="https://www.linkedin.com/in/zadid-taqwa-21a755170/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn />{" "}
+          </a>
         </div>
       </div>
     </>
